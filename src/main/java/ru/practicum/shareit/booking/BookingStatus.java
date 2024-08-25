5ad9ce7bc2a1da4ac0be22dce798342c8438b2de
Kirill <doroshenkokirill56@yandex.ru>
2024-08-26 00:32:23 +0300

    fix: Booking

--- src/main/java/ru/practicum/shareit/booking/BookingStatus.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.booking;
-
-public enum BookingStatus {
-    WAITING, // новое бронирование, ожидает одобрения
-    APPROVED, // бронирование подтверждено владельцем
-    REJECTED, // бронирование отклонено владельцем
-    CANCELED //бронирование отменено создателем
-}
