f50065a211b0159e607f598eb224b9eac8d53588
Kirill <doroshenkokirill56@yandex.ru>
2024-09-11 20:43:52 +0300

    fix: исправил логику работы метода getItemDtosToRequestList

--- server/src/main/java/ru.practicum.shareit/item/repository/ItemRepository.java
+++ server/src/main/java/ru.practicum.shareit/item/repository/ItemRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
+import java.util.Set;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-    List<Item> findItemsByRequestId(int requestId);
+    List<Item> findItemsByRequestIdIn(Set<Integer> itemIds);
+
 }
--- server/src/main/java/ru.practicum.shareit/request/service/ItemRequestServiceImpl.java
+++ server/src/main/java/ru.practicum.shareit/request/service/ItemRequestServiceImpl.java
@@ -1,9 +1,12 @@
 package ru.practicum.shareit.request.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.comment.repository.CommentRepository;
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.model.ItemMapper;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
@@ -15,13 +18,18 @@ import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ItemRequestServiceImpl implements ItemRequestService {
     private final ItemRepository itemRepository;
     private final UserRepository userRepository;
     private final ItemRequestRepository itemRequestRepository;
+    private final CommentRepository commentRepository;
 
     @Override
     public List<ItemRequestDto> getAll(int userId) {
@@ -70,17 +78,27 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     }
 
     private List<ItemRequestDto> getItemDtosToRequestList(List<ItemRequest> requests) {
+        Set<Integer> requestIds = requests.stream()
+                .map(ItemRequest::getId)
+                .collect(Collectors.toSet());
+        log.info("Список всех requestIds: {}", requestIds);
+
+        List<Item> items = itemRepository.findItemsByRequestIdIn(requestIds);
+
+        Map<Integer, List<ItemDto>> itemsByRequestsId = items.stream()
+                .map(ItemMapper::toItemDto)
+                .collect(Collectors.groupingBy(ItemDto::getRequestId));
+        log.info("Сгруппированные по RequestsId: {}", itemsByRequestsId);
+
         return requests.stream()
                 .map(ItemRequestMapper::toItemRequestDto)
                 .peek(requestDto -> {
-                    System.out.println("Request ID: " + requestDto.getId());
-                    List<ItemDto> itemDtoList = itemRepository.findItemsByRequestId(requestDto.getId())
-                            .stream()
-                            .map(ItemMapper::toItemDto)
-                            .toList();
-                    System.out.println("Items found for Request ID " + requestDto.getId() + ": " + itemDtoList.size());
+                    int requestId = requestDto.getId();
+                    log.info("RequestId: {}", requestId);
+                    List<ItemDto> itemDtoList = itemsByRequestsId.getOrDefault(requestId, List.of());
+                    log.info("Items найденные по RequestId {}: {}", requestId, itemDtoList);
                     requestDto.setItems(itemDtoList);
                 })
-                .toList();
+                .collect(Collectors.toList());
     }
 }
